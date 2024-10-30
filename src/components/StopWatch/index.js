b8339fee1d1f04d14fd90bc7b72e92e990c435b1
Balaji <chokkarabalaji@gmail.com>
2024-10-30 13:12:11 +0530

    secondCommit

--- src/components/StopWatch/index.js
+++ src/components/StopWatch/index.js
@@ -2,38 +2,50 @@ import React, { Component } from 'react';
 import './index.css';
 
 class StopWatch extends Component {
-    state={time:0,seconds:0,isRunning:false}
-    startTimer=()=>{
-        if(!this.state.isRunning){
-       this.intervalId=setInterval(()=>{
-            this.setState(prevstate=>{
-                const{time,seconds}=prevstate
-                if(seconds===59)
-                {
-                    return({time:time+1,seconds:0})
-                }
-                else{
-                    return({seconds:seconds+1})
-                }
-            })
-       },1000)
-       this.setState({isRunning:true})
+    state = { time: 0, seconds: 0, isRunning: false };
+
+    componentDidMount() {
+        console.log("componentDidMount");
     }
+
+    startTimer = () => {
+        if (!this.state.isRunning) {
+            this.intervalId = setInterval(() => {
+                this.setState(prevState => {
+                    const { time, seconds } = prevState;
+                    if (seconds === 59) {
+                        return { time: time + 1, seconds: 0 };
+                    } else {
+                        return { seconds: seconds + 1 };
+                    }
+                });
+            }, 1000);
+            this.setState({ isRunning: true });
+        }
+    };
+
+    resetTimer = () => {
+        clearInterval(this.intervalId);
+        this.setState({ time: 0, seconds: 0, isRunning: false });
+    };
+
+    stopTimer = () => {
+        clearInterval(this.intervalId);
+        this.setState({ isRunning: false });
+    };
+
+    componentWillUnmount() {
+        clearInterval(this.intervalId);
+        console.log("unmount");
     }
-    resetTimer=()=>{
-        clearInterval(this.intervalId)
-        this.setState({time:0,seconds:0,isRunning:false})
-    }
-    stopTimer=()=>{
-        clearInterval(this.intervalId)
-        this.setState({isRunning:false})
-    }
-    
+
     render() {
-        const{time,seconds}=this.state
-        console.log(time,seconds)
-        const formattedMinutes=time>9?time:`0${time}`
-         const formattedSeconds=seconds>9?seconds:`0${seconds}`
+        const { time, seconds } = this.state;
+        console.log("rendering");
+
+        const formattedMinutes = String(time).padStart(2, '0');
+        const formattedSeconds = String(seconds).padStart(2, '0');
+        
         return (
             <div className="stopwatch-container">
                 <h1 className="title">Stopwatch</h1>
@@ -42,7 +54,13 @@ class StopWatch extends Component {
                     <p className="time-display">{formattedMinutes}:{formattedSeconds}</p>
                 </div>
                 <div className="buttons">
-                    <button className="start-button" onClick={this.startTimer}>Start</button>
+                    <button 
+                        className="start-button" 
+                        onClick={this.startTimer} 
+                        disabled={this.state.isRunning}
+                    >
+                        Start
+                    </button>
                     <button className="stop-button" onClick={this.stopTimer}>Stop</button>
                     <button className="reset-button" onClick={this.resetTimer}>Reset</button>
                 </div>
